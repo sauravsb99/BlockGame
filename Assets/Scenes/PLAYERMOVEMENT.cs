90aeef550501888ac8665b2b3c14565cca170e0a
Saurav S B <32162836+sauravsb99@users.noreply.github.com>
2019-03-24 17:04:35 +0530

    Add files via upload
    Assets

---
+++ Assets/Scenes/PLAYERMOVEMENT.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+
+public class PLAYERMOVEMENT : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float ForwardForce = 2000f;
+    public float SidewaysForce = 500f;
+
+
+    
+    void Start()
+    {
+        //rb.AddForce(0, 500, 200);
+        //rb.useGravity = false;
+
+    }
+        
+ void FixedUpdate()
+      {
+        rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(SidewaysForce * Time.deltaTime, 0, 0 , ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(- SidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ Assets/Scenes/collision.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public PLAYERMOVEMENT movement;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag =="Obstacle")
+        {
+            movement.enabled = false;
+        }
+
+    }
+
+
+
+
+
+}
+
+
